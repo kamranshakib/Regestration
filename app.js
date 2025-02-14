a659d80953e61e4f6df13f7b6243026f29c3055a
Kamran Shakib <kamranshakib371@gmail.com>
2025-02-14 09:58:58 -0800

    create a new ejs file and search students with it's class or teacher name

--- app.js
+++ app.js
@@ -1,67 +1,98 @@
-const express = require('express')
-const ejs = require('ejs')
-const bodyParser = require('body-parser')
-const multer = require('multer')
-const {StudentDB, teacherDB} = require('./model/mongo')
-const app = express()
-app.use(bodyParser.urlencoded({extended:true}))
-app.set('view engine' ,'ejs')
-app.use(express.static('public'))
-const upload = multer({dest: 'public/image/'})
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const multer = require("multer");
+const { StudentDB, teacherDB } = require("./model/mongo");
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+const upload = multer({ dest: "public/image/" });
 
-app.get('/',(req,res)=>{
-    res.render('menu')
-})  
-    //   برای استفاده از برنامه  body parser , ejs , mongoose , express , multer را نصب کنید
+app.get("/", (req, res) => {
+  res.render("menu");
+});
+//   برای استفاده از برنامه  body parser , ejs , mongoose , express , multer را نصب کنید
 
- 
-                                        //  نشان دادن صفحه ثبت نام شاگردان **
+//  نشان دادن صفحه ثبت نام شاگردان **
 
-app.get('/regestrationStudent',(req,res)=>{
-    res.render('studentRegestration')
+app.get("/searchStudent", (req, res) => {
+  res.render("searchStudent");
+});
 
+app.post("/searchStudents", (req, res) => {
+  const { teacherSearchStudents, classesSearch, nameSearch } = req.body; // teacher_name:`${teacherSearchStudents}`,// class_student: `${classesSearch}
 
+  StudentDB.find({ class_student: `${classesSearch}` })
+    .then((result) => res.render("tableOFStudents", { students: result }))
+    .catch((err) => console.log(err));
+
+});
+
+app.get('/tableOFStudentss',(req,res)=>{
+    const { teacherSearchStudents, classesSearch, nameSearch } = req.body; // teacher_name:`${teacherSearchStudents}`,// class_student: `${classesSearch}
+
+    StudentDB.find({ class_student: `${classesSearch}` })
+      .then((result) => res.render("tableOFStudents", { students: result }))
+      .catch((err) => console.log(err));
 })
 
-                                //       **  ثبت نام شاگردان در دیتایس***** 
-app.post('/RegestrationStudent',upload.single('photo_student'),(req,res)=>{
+// Search Students
 
-   const studentInfo = new StudentDB(req.body)  
-   const photo = `/image/${req.file.filename}`;
-    
-   studentInfo.photo_student = photo;
-   studentInfo.save()
-   .then((result)=> console.log('save data'))
-   .catch((err)=> console.log(err))
-    res.redirect('/regestrationStudent')
-}) 
+app.get("/regestrationStudent", (req, res) => {
+  res.render("studentRegestration");
+});
 
- 
+app.get("/showStudents/:id", (req, res) => {
+  const id = req.params.id;
+
+  StudentDB
+  .find({_id : `${id}`})
+  .then((result) => res.render('showStudent' , {result}) )
+  .catch((err) => console.log(err));
+});
+
+//       **  ثبت نام شاگردان در دیتایس*****
+app.post("/RegestrationStudent", upload.single("photo_student"), (req, res) => {
+  const studentInfo = new StudentDB(req.body);
+  const photo = `/image/${req.file.filename}`;
+
+  studentInfo.photo_student = photo;
+  studentInfo
+    .save()
+    .then((result) => console.log("save data"))
+    .catch((err) => console.log(err));
+  res.redirect("/regestrationStudent");
+});
+
+
+
+
+
+
+
 // app.get('/findStudents',(req,res)=>{
 //     res.render('showStudent')
 // })
-     
-  
+
 //            **                      **   پیدا کردن شاگردان از دیتابیس از طریق نام **
-app.post('/findStudent',(req,res)=>{
-    const nameStudent = req.body.search;
-    StudentDB.find(
-            {
-                    name_student: `${nameStudent}`
-                }
-            )
-            .then((result)=>res.render('showStudent',{studentInfo:result}))
-            .catch((err)=>console.log(err))
-})
- 
+// app.post('/findStudent',(req,res)=>{
+//     const nameStudent = req.body.search;
+//     StudentDB.find(
+//             {
+//                     name_student: `${nameStudent}`
+//                 }
+//             )
+//             .then((result)=>res.render('showStudent',{studentInfo:result}))
+//             .catch((err)=>console.log(err))
+// })
 
-                                //  teacher 
-app.get('/regestrationTeacher',(req,res)=>{
-    res.render('teacherRegestration')
-})
+//  teacher
+app.get("/regestrationTeacher", (req, res) => {
+  res.render("teacherRegestration");
+});
 
-
-                                //   ثبت نام کردن استاد  
+//   ثبت نام کردن استاد
 
 // app.post('/Regester_teacher', upload.single('photo_teacher'),(req,res)=>{
 //     const teacherInfo = new teacherDB(req.body)
@@ -74,12 +105,6 @@ app.get('/regestrationTeacher',(req,res)=>{
 //     res.redirect('/teacher')
 // })
 
-
-
-
-
-
-
-app.listen(3000,()=>{
-    console.log('Server on port 3000')
-})
+app.listen(3000, () => {
+  console.log("Server on port 3000");
+});
